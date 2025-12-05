0647555b6c3482f2f9eaef33b7e404fb2221c027
Alesim <calejandrito317@gmail.com>
2025-12-05 15:42:58 -0500

    Fix import path for GalleryController

--- routes/Admin/GalleryRoutes.js
+++ routes/Admin/GalleryRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
-const galleryController = require("../../controllers/galleryController");
+const galleryController = require("../../controllers/GalleryController");
 
 const router = express.Router();
 
